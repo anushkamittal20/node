5a02988c22e87ed47263c26250994703a0ab9f83
briha <briha@brihas-MacBook-Pro.local>
2022-12-09 17:29:23 +0530

    routes added. server not tested yet

---
+++ api/routes/index.js
@@ -0,0 +1,29 @@
+var express = require("express");
+var router = express.Router();
+var appController = require("../controller/main.controller");
+
+router.route("/chooseService").get(appController.chooseService);
+
+router.route("/mainController").get(appController.mainController);
+
+router
+  .route("/userLocatedWithinRadius")
+  .get(appController.userLocatedWithinRadius);
+
+router.route("/nearestAmbulance").get(appController.nearestAmbulances);
+
+// router.route("/decodeWaypoints").get(appController.decodeWaypoints);
+
+// router
+//   .route("/dynamicRadiusGenerator")
+//   .get(appController.dynamicRadiusGenerator);
+
+// router.route("/generateWaypoints").get(appController.generateWaypoints);
+
+// router.route("/getUserLocation").get(appController.getUserLocation);
+
+// router
+//   .route("/waypointsToCoordinates")
+//   .get(appController.waypointsToCoordinates);
+
+module.exports = router;
