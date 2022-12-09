687283b0798f94080cef9a9a37face3dc01fc0cf
briha <briha@brihas-MacBook-Pro.local>
2022-12-09 17:20:10 +0530

    server mod

--- server.js
+++ server.js
@@ -1,54 +1,26 @@
 var http = require("http");
-
-/**
- * Module dependencies.
- */
-
 var app = require("./app.js");
-/**
- * Get port from environment and store in Express.
- */
-
 var port = normalizePort(process.env.PORT || "3000");
 app.set("port", port);
-
-/**
- * Create HTTP server.
- */
-
 var server = http.createServer(app);
-
-/**
- * Listen on provided port, on all network interfaces.
- */
 
 server.listen(port);
 server.on("error", onError);
 server.on("listening", onListening);
-console.log("Connected to Server to Port : 3000");
-/**
- * Normalize a port into a number, string, or false.
- */
 
 function normalizePort(val) {
   var port = parseInt(val, 10);
 
   if (isNaN(port)) {
-    // named pipe
     return val;
   }
 
   if (port >= 0) {
-    // port number
     return port;
   }
 
   return false;
 }
-
-/**
- * Event listener for HTTP server "error" event.
- */
 
 function onError(error) {
   if (error.syscall !== "listen") {
@@ -57,7 +29,6 @@ function onError(error) {
 
   var bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
 
-  // handle specific listen errors with friendly messages
   switch (error.code) {
     case "EACCES":
       console.error(bind + " requires elevated privileges");
@@ -71,10 +42,6 @@ function onError(error) {
       throw error;
   }
 }
-
-/**
- * Event listener for HTTP server "listening" event.
- */
 
 function onListening() {
   var addr = server.address();
