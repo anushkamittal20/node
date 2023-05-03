ca091e9f890e150014ccd4b499caca0d26887322
anusha-c18 <anushac2001@gmail.com>
2023-05-03 19:36:33 +0530

    Removed unnecessary functions

--- API Gateway/main.controller.js
+++ API Gateway/main.controller.js
@@ -1,121 +1,3 @@
-// module.exports.chooseService = function (req, res) {
-//   /*
-//     Request has to be of the form:
-//     {
-//     "serviceName": "ambulance",
-//     "position": {
-//         "coords" : {
-//             "latitude" : 13.0147,
-//             "longitude": 77.581
-//         }
-//     },
-//     "to" : "THIS_PLACE"
-//     }
-//     */
-//   console.log("chooseService is called ");
-
-//   // getting values from postman/client
-//   let GETchosenService = req.body.serviceName;
-//   let GETPosition = req.body.position;
-//   let GETTo = req.body.to;
-
-//   // creating a chooseService object
-//   const choose_service = require("./chooseService.controller");
-//   // calling functions
-//   choose_service.chooseService("", GETchosenService);
-//   choose_service.confirmService();
-//   // choose_service.getLocation();
-//   choose_service.showPosition(GETPosition);
-//   choose_service.goTo(GETTo);
-
-//   // returning whatever we've received so far
-//   let returnData = {};
-//   returnData.message = "chooseService is called";
-//   returnData.selectedService = GETchosenService;
-//   returnData.position = GETPosition;
-//   returnData.to = GETTo;
-
-//   res.status(200).json(returnData);
-// };
-
-// module.exports.decodeWaypoints = function (req, res) {
-//   /*
-//   request has to be of the form:
-//   {
-//   "str" : "SOME_STRING",
-//   "precision": 5
-//   }
-//   */
-//   console.log("decodeWaypoints is called ");
-
-//   // getting values from postman/client
-//   let GETPolyline = req.body.polyline;
-
-//   // creating a decodeWaypoints object
-//   const decode_waypoints = require("./decode_waypoints.controller");
-//   // calling functions
-//   let decodedWaypoints = decode_waypoints.decodePath(GETPolyline);
-
-//   // returning whatever we've received so far
-//   let returnData = {};
-//   returnData.message = "decodeWaypoints is called";
-//   returnData.decodedWaypoints = decodedWaypoints;
-
-//   res.status(200).json(returnData);
-// };
-
-// module.exports.dynamicRadiusGenerator = function (req, res) {
-//   console.log("dynamicRadiusGenerator is called ");
-
-//   let GETWaypoints = req.body.waypoints;
-//   // let GETLat1 = req.body.lat1;
-//   // let GETLat2 = req.body.lat2;
-//   // let GETLong1 = req.body.long1;
-//   // let GETLong2 = req.body.long2;
-
-//   const dynamic_radius_generation = require("./dynamic_radius_generation.controller");
-//   let minDistance = dynamic_radius_generation.findMinDistance(GETWaypoints);
-//   // let distance = dynamic_radius_generation.distance(
-//   //   GETLat1,
-//   //   GETLat2,
-//   //   GETLong1,
-//   //   GETLong2
-//   // );
-
-//   let returnData = {};
-//   returnData.message = "dynamicRadiusGenerator is called";
-//   returnData.minDistance = minDistance;
-//   returnData.distance = distance;
-
-//   res.status(200).json(returnData);
-// };
-/*
-  module.exports.generateWaypoints = function (req, res) {
-    console.log("generateWaypoints is called ");
-  
-    let GETPolyline = req.body.polyline;
-    let GETPrecision = req.body.precision;
-  
-    const decode_waypoints = require("./decode_waypoints.controller");
-    let decodedPolyline = decode_waypoints.decodePath(GETPolyline, GETPrecision);
-  
-    const generate_waypoints = require("./generate_waypoints.controller");
-  
-    let generatedWaypoints =
-      generate_waypoints.generateWaypoints(decodedPolyline);
-  
-    console.log("these are the decoded waypoints: ", decodedPolyline);
-    console.log("these are the generated waypoints: ", generatedWaypoints);
-  
-    let returnData = {};
-    returnData.message = "generateWaypoints is called";
-    returnData.generatedWaypoints = generatedWaypoints;
-    returnData.decodedWaypoints = decodedPolyline;
-  
-    res.status(200).json(returnData);
-  };
-  */
-
 module.exports.userLocatedWithinRadius = function (req, res) {
   const targetAppUrl = "http://10.45.129.27:3000";
   let GETCurrentLocation = req.body.currentLocation;
@@ -175,20 +57,6 @@ module.exports.userLocatedWithinRadius = function (req, res) {
   res.status(200).json(returnData);
 };
 
-// module.exports.waypointsToCoordinates = function (req, res) {
-//   console.log("waypointsToCoordinates is called ");
-
-//   const waypoints_to_coordinates = require("./waypoints_to_coordinates.controller");
-//   // waypoints_to_coordinates.convertWaypointstoCoordinates()s
-
-//   let returnData = {};
-//   returnData.status = "200";
-//   returnData.message = "waypointsToCoordinates is called";
-//   returnData.error = "NA";
-
-//   res.status(200).json(returnData);
-// };
-
 module.exports.mainController = function (req, res) {
   let GETPolyline = req.body.polyline;
 
@@ -205,12 +73,6 @@ module.exports.mainController = function (req, res) {
     immediate_waypoints.immediateWaypoints(generatedWaypoints);
 
   let maxRadius = minRadius / 2 + minRadius;
-
-  // console.log("generateWaypoints is called ");
-  // console.log("these are the decoded waypoints: ", decodedPolyline);
-  // console.log("these are the generated waypoints: ", generatedWaypoints);
-  // console.log("dynamicRadiusGenerator is called ");
-  // console.log("immediateWaypoints is called");
 
   let returnData = {};
   returnData.dynamic_radius = minRadius;
@@ -252,35 +114,10 @@ module.exports.notify = function (req, res) {
   returnData.message = "userLocatedWithinRadius is called123";
   returnData.userConfirmation = userConfirmation;
   res.send(userConfirmation);
-
-  //res.status(200).json(returnData);
 };
-/*let GETUserLng = req.body.Lat;
-  let GETUserLat = req.body.Lng;*/
-
-// userConfirmation = true;
-//res.send(userConfirmation);
-//res.send(GETUserLat);
-//let returnData = {};
-//returnData.L = GETUserLat;
-
-//  res.status(200);
-
-// };
 
 module.exports.ambulanceReturn = function (req, res) {
-  // let origin={};
-  // origin.lat = GETCurrentLocation.lat;
-  // origin.lng=GETCurrentLocation.lng;
-  // res.JSON(origin);
   let destination = req.body.destination;
-  // let vals=Object.values(GETCurrentLocation);
-  // const obj = JSON.stringify(GETCurrentLocation);
-  // console.log(obj.GETCurrentLocation.lat);
-
-  // var GETAmbulanLocation=req.body.ambulanceLocation;
-  // var origin = GETCurrentLocation.lat.concat(GETCurrentLocation.lng);
-  // var destination=GETAmbulanLocation.lat.concat(GETAmbulanLocation.lng);
   var axios = require("axios");
 
   var config = {
@@ -305,26 +142,11 @@ module.exports.ambulanceReturn = function (req, res) {
     .catch(function (error) {
       console.log(error);
     });
-
-  // let eta=response.rows[0].elements[0].duration_in_traffic.text;
-  // console.log(eta);
-
-  //return eta and user location
 };
 
 module.exports.notify2 = function (req, res) {
   let returnData = {};
-  // returnData.message = "notify2 for ambulance is called123";
   returnData.userConfirmation = userConfirmation;
-  //users location
-  // if(!userConfirmation) {
-  //   returnData.currentLocation = {"lat":0.0,"lng":0.0};
-  // }else{
-  // returnData.currentLocation = GETCurrentLocation;
-  // }
   userConfirmation = false;
-
-  //return user location
-
   res.status(200).json(returnData);
 };
